fd17250304b01996221f391175d361719ef3d71f
Alasdair Monk <alasdair.monk@gmail.com>
2025-06-10 11:45:00 +0100

    Initial Commit

---
+++ Ports/PortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortsApp.swift
+//  Ports
+//
+//  Created by al on 10/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
